cfe22c42728b9aaa99d4c53a34463dadfa99ff38
Kevin Tena <kevten09@gmail.com>
2018-10-12 15:02:58 -0500

    Ex2 tests passed(fix)

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,5 +1,27 @@
 def reconstruct_trip(tickets):
-  pass 
+  table = {}
+  for i in tickets:
+    table[i[0]] = i[1]
+  
+  result = []
+  
+  try:
+    link = table[None]
+    result.append(link)
+    complete = False
+
+    while(complete == False):
+        link = table[link]
+        if(link):
+          result.append(link)
+        if(link == None):
+          complete = True
+    
+    return result
+    
+   except:
+    return []
+
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
